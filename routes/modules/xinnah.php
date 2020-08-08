5b0a5a14b931c8aa8a603b808c00136ffb47a822
zinnah997 <zinnah997@gmail.com>
2020-08-08 10:00:37 +0600

    no

--- routes/modules/xinnah.php
+++ routes/modules/xinnah.php
@@ -1,12 +1,12 @@
 <?php
 
-
 Route::group(['prefix' => 'recruitment','namespace' => 'Recruitment'], function(){
 	Route::resource('recruit', 'RecruitController');
 	Route::post('first-step-recruitment', 'RecruitController@basicRecruitStore');
 	Route::post('second-step-recruitment', 'RecruitController@medicalRecruitStore');
 });
 
+// common routes
 Route::get('employee-type-wise-designation/{id}', 'Common\EmployeeAttributeController@getDesignation');
 Route::get('area-wise-department/{id}', 'Common\EmployeeAttributeController@getDepartment');
 Route::get('department-wise-section/{id}', 'Common\EmployeeAttributeController@getSection');
