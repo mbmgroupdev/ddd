38d44c3369946fb28a1cce97cd16a988036f797b
Md. Rakibul Islam <rakib1708@gmail.com>
2020-08-30 17:33:47 +0600

    recruit

--- app/Http/Controllers/Hr/Adminstrator/RolesController.php
+++ app/Http/Controllers/Hr/Adminstrator/RolesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Auth, Validator, DB; 
 
 class RolesController extends Controller
 {
@@ -31,59 +32,106 @@ class RolesController extends Controller
         return view('hr.adminstrator.add-roles', compact('permissions'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   /**
+    * Store a newly created Role in storage.
+    *
+    * @param  \App\Http\Requests\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(Request $request)
-    {
-        //
+    {     
+            
+        //dd($request->all());
+        $validator = Validator::make($request->all(),[ 
+            'name' => 'required|string|max:255|unique:roles,name,'. $request->id,
+        ]);  
+
+        if ($validator->fails())
+        { 
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $role = new Role();
+        $role->name = $request->name;
+        $role->hierarchy = $request->hierarchy;
+        $role->save(); 
+
+        if($request->permissions != null){
+            $role->givePermissionTo($request->permissions);
+            log_file_write($role->name ." - role created: ", $role->id);
+
+            return redirect('hr/adminstrator/edit/role/'.$role->id)
+                ->with("success", "Save Successful!");
+        }
+             
+    }
+    
+
+    public function edit($id, Request $request)
+    {  
+        $role = Role::findOrFail($id); 
+
+        $permissions = Permission::get();
+        $permissions = $permissions->groupBy(['module','groups']);
+
+
+        return view('hr.adminstrator.edit_role', compact('role','permissions'));
+
+
     }
 
+    public function update(Request $request)
+    {     
+            
+        $validator = Validator::make($request->all(),[ 
+            'name' => 'required|string|max:255',
+        ]);  
+
+        if ($validator->fails())
+        { 
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+       
+            
+        $role = Role::findOrFail($request->id);
+        $update = $role->update([
+            'name' => $request->name,
+            'hierarchy' => $request->hierarchy
+        ]);
+        
+        $permissions = $request->permissions ? $request->permissions : [];
+        $role->syncPermissions($permissions);
+
+        log_file_write("Roles Updated", $request->id);
+
+        return redirect()->back()->with("success", "Update Successful!");
+             
+    }
+
+
     /**
-     * Display the specified resource.
+     * Remove Role from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+    public function destroy(Request $request)
+    {     
+        
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        $role = Role::findOrFail($request->id);
+        if ($role->delete())
+        {
+            log_file_write($role->name."- role deleted", $request->id);
+            
+            return back()
+                ->with("success", "Delete Successful!");
+        }
+        else
+        {
+            return back()
+                    ->with("error", "Please try again.");
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        }
     }
 }
