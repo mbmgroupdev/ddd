a34aee46720b8d5a1c4632465e16a89f266f2293
zinnah997 <zinnah997@gmail.com>
2020-09-05 17:15:44 +0600

    css change

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware' => 'guest'], function(){
-	Route::get('/', function () {
-	    return view('login');
-	});
+	Route::get('/', 'HomeController@login');
 });
-Route::get('/clear-cache', function () {
-    $exitCode = Artisan::call('config:clear');
-    $exitCode = Artisan::call('cache:clear');
-    //$exitCode = Artisan::call('route:clear');
-    $exitCode = Artisan::call('config:cache');
-    //$exitCode = Artisan::call('route:cache');
-    return 'DONE'; //Return anything
-});
+Route::get('/clear-cache', 'HomeController@clear');
 
 Auth::routes();
 
